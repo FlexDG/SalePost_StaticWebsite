488d747637c4e6d6a81b1d7964a095cf64bc9f02
Felix Rafael De Guzman <deguzman.felixrafael@auf.edu.ph>
2024-07-30 06:00:06 +0800

    day 7 commit

--- salepost/assets/js/index.js
+++ salepost/assets/js/index.js
@@ -48,6 +48,54 @@ $(document).ready(function(){
 
    });
 
+   
+   
+
+// deals slider
+
+   $(document).ready(function(){
+
+    $('.deals-slider').slick({
+       dots: true,
+       arrows: false,
+       infinite: true,
+       speed: 300,
+       slidesToShow: 1,
+       slidesToScroll: 1,
+       autoplay: true,
+       prevArrow: "<button type='button' class='slick-prev pull-left'><i class='bi bi-chevron-left'></i></button>",
+       nextArrow: "<button type='button' class='slick-next pull-right'><i class='bi bi-chevron-right'></i></button>",
+       responsive: [
+         {
+           breakpoint: 1025,
+           settings: {
+             slidesToShow: 2,
+             slidesToScroll: 1,
+             infinite: true,
+           }
+         },
+         {
+           breakpoint: 769,
+           settings: {
+             slidesToShow: 1,
+             slidesToScroll: 1,
+             arrows: false,
+           }
+         },
+         {
+           breakpoint: 500,
+           settings: {
+             slidesToShow: 1,
+             slidesToScroll: 1,
+             arrows: false,
+           }
+         }
+       ]
+     });
+
+
+   });
+
 
 // banner-slider 2
 
